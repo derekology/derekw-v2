faf635179ab5168e4a5de9c47261db7b4a823015
Derek Woo <me@derekw.co>
2023-02-17 21:41:27 -0800

    Create components for desktop and mobile menu

---
+++ src/components/PersistentNavMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div id="sidebar">
+      <nav>
+        <NavSidebar />
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+import NavSidebar from "@/components/NavSidebar.vue";
+
+export default {
+  name: "PersistentNavMenu",
+  components: {
+    NavSidebar,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/SlideNavMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div
+      class="offcanvas offcanvas-start"
+      tabindex="-1"
+      id="sidebar-collapse"
+      aria-labelledby="sidebarLabel"
+    >
+      <div id="sidebar-collapse">
+        <NavSidebar />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+import NavSidebar from "@/components/NavSidebar.vue";
+
+export default {
+  name: "SlideNavMenu",
+  components: {
+    NavSidebar,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
