46cb712e35d0e670fdb798568351a4ab6b9df850
Derek Woo <me@derekw.co>
2023-02-17 21:41:39 -0800

    Create component for mobile top navbar

---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav>
+      <div class="navbar navbar-dark bg-dark d-md-none">
+        <button
+          class="btn btn-primary ms-1"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#sidebar-collapse"
+          aria-controls="sidebar-collapse"
+        >
+          <span class="glyphicon glyphicon-menu-hamburger">Menu</span>
+        </button>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+
+export default class TopNavBar extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
