5cb0c8f0c8de4b04e758607056ae7d294fad94d2
Derek Woo <me@derekw.co>
2023-02-19 02:11:16 -0800

    Move horizontal menu into separate component

--- src/components/NavSidebar.vue
+++ src/components/NavSidebar.vue
@@ -1,70 +1,26 @@
 <template>
   <div
     id="sidebar"
-    class="d-flex flex-column flex-shrink-0 p-3 text-white vh-100 position-fixed"
+    class="d-flex flex-column flex-shrink-0 text-white vh-100 position-fixed"
   >
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item mt-2">
-        <router-link
-          to="/"
-          class="nav-link text-white text-start"
-          aria-current="page"
-        >
-          Home
-        </router-link>
-      </li>
-      <li class="nav-item mt-2">
-        <router-link to="/about" class="nav-link text-white text-start">
-          About
-        </router-link>
-      </li>
-    </ul>
-    <hr />
+    <PostFeed class="mt-2" />
 
-    <PostFeed />
+    <hr class="mb-0" />
 
-    <hr />
-    <div class="dropdown">
-      <a
-        href="#"
-        class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-        id="dropdownUser1"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        <img
-          src="https://github.com/mdo.png"
-          alt=""
-          class="rounded-circle me-2"
-          width="32"
-          height="32"
-        />
-        <strong>mdo</strong>
-      </a>
-      <ul
-        class="dropdown-menu dropdown-menu-dark text-small shadow"
-        aria-labelledby="dropdownUser1"
-      >
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li>
-          <hr class="dropdown-divider" />
-        </li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
+    <HorizontalMenu />
   </div>
 </template>
 
 <script>
 import { Vue } from "vue-class-component";
 import PostFeed from "@/components/PostFeed.vue";
+import HorizontalMenu from "@/components/HorizontalMenu.vue";
 
 export default {
   name: "NavSidebar",
   components: {
     PostFeed,
+    HorizontalMenu,
   },
 };
 </script>
@@ -75,12 +31,5 @@ export default {
   background-color: #222222;
   border-top: 5px #dd3333 solid;
   width: 250px;
-}
-a.router-link-active {
-  background-color: #ffffff17;
-}
-
-a:hover {
-  background-color: #ffffff08;
 }
 </style>
