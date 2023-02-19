16186d74c7be1eb6fbf7b5a9fbc611340120893f
Derek Woo <me@derekw.co>
2023-02-19 02:09:37 -0800

    Create component for single post content

---
+++ src/components/SinglePostContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ content }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    content: String,
+  },
+})
+export default class SinglePostContent extends Vue {
+  content!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
