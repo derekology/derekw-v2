3fe42930bd26aabe894a8c843f922e4b23a3c2af
Derek Woo <me@derekw.co>
2023-02-19 02:09:51 -0800

    Create component for single post view

---
+++ src/views/PostView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <SinglePostTitle :title="postTitle" />
+    <SinglePostContent :content="postContent" />
+  </div>
+</template>
+
+<script>
+import SinglePostTitle from "@/components/SinglePostTitle.vue"; // @ is an alias to /src
+import SinglePostContent from "@/components/SinglePostContent.vue"; // @ is an alias to /src
+
+export default {
+  name: "PostView",
+
+  components: {
+    SinglePostTitle,
+    SinglePostContent,
+  },
+
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      postTitle: this.post.title,
+      postContent: this.post.content,
+    };
+  },
+};
+</script>
