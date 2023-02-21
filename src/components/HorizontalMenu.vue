a0f82e3a240d7347499cde817d53ab9080d02196
Derek Woo <me@derekw.co>
2023-02-20 23:13:22 -0800

    Update horizontal menu items

--- src/components/HorizontalMenu.vue
+++ src/components/HorizontalMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav nav-pills flex-row mb-auto row">
-    <div class="nav-item col pe-0">
+    <!-- <div class="nav-item col pe-0">
       <router-link
         to="/"
         class="nav-link text-white text-start text-center"
@@ -8,13 +8,10 @@
       >
         Home
       </router-link>
-    </div>
+    </div> -->
     <div class="nav-item col ps-0">
-      <router-link
-        to="/about"
-        class="nav-link text-white text-starttext-center"
-      >
-        About
+      <router-link to="/" class="nav-link text-white text-starttext-center">
+        About / Contact
       </router-link>
     </div>
   </div>
@@ -23,12 +20,9 @@
 <script>
 import { Vue } from "vue-class-component";
 
-export default {
-  name: "HorizontalMenu",
-};
+export default class HorizontalMenu extends Vue {}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .nav-link {
   border-radius: 0px !important;
