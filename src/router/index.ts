c4d22a5b7822120dd31a89d70228a35a615d7850
Derek Woo <me@derekw.co>
2023-02-20 23:20:09 -0800

    Remove About page path and test prop data

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,22 +11,16 @@ const routes: Array<RouteRecordRaw> = [
     path: "/:title",
     name: "posts",
     component: () => import("../views/PostView.vue"),
-    props: {
-      post: {
-        title: "test title",
-        content: "test content",
-      },
-    },
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  // },
 ];
 
 const router = createRouter({
