944f3ea52aeace74d912b0507abff609296a9cdf
Derek Woo <me@derekw.co>
2023-03-02 03:08:50 -0800

    Add PersistentNavMenu.vue

---
+++ src/components/navigation/PersistentNavMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div id="sidebar">
+      <nav>
+        <NavSidebar />
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavSidebar from "@/components/navigation/NavSidebar.vue";
+
+export default defineComponent({
+  name: "PersistentNavMenu",
+  components: {
+    NavSidebar,
+  },
+});
+</script>
+
+<style scoped></style>
