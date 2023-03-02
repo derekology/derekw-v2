47ac25522bdf935c7f5c42b92d2aee9895976fbb
Derek Woo <me@derekw.co>
2023-03-02 03:07:59 -0800

    Add ContentLoader.vue

---
+++ src/components/global/ContentLoader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ContentLoader",
+
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+div {
+  font-style: italic;
+}
+</style>
