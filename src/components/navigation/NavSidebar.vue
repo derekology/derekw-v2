ba1f0f4c145c753478433817e5192b813730219a
Derek Woo <me@derekw.co>
2023-03-02 03:08:27 -0800

    Add NavSidebar.vue

---
+++ src/components/navigation/NavSidebar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="sidebar" class="d-flex flex-column flex-shrink-0 text-white">
+    <div class="d-md-none offcanvas-header pt-0 pb-0">
+      <h5 class="offcanvas-title" id="sidebarLabel">
+        <router-link
+          to="/"
+          class="nav-link text-white text-start text-center d-md-none"
+          aria-current="page"
+          ><HeaderLogo />
+        </router-link>
+      </h5>
+      <button
+        type="button"
+        class="btn-close text-reset me-2"
+        data-bs-dismiss="offcanvas"
+        aria-label="close"
+      ></button>
+    </div>
+    <div>
+      <!-- <router-link
+        to="/"
+        class="nav-link text-white text-start text-center d-none d-md-block"
+        aria-current="page"
+        ><HeaderLogo />
+      </router-link> -->
+      <span>Latest Posts</span>
+    </div>
+
+    <PostFeed class="mt-0" />
+
+    <hr class="mb-0" />
+
+    <HorizontalMenu />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PostFeed from "@/components/navigation/PostFeed.vue";
+import HorizontalMenu from "@/components/navigation/HorizontalMenu.vue";
+import HeaderLogo from "@/components/global/HeaderLogo.vue";
+
+export default defineComponent({
+  name: "NavSidebar",
+  components: {
+    PostFeed,
+    HorizontalMenu,
+    HeaderLogo,
+  },
+
+  updated() {
+    this.getHeight();
+  },
+
+  methods: {
+    getHeight: function () {
+      let vh = window.innerHeight * 0.01;
+
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    },
+  },
+});
+</script>
+
+<style scoped>
+#sidebar {
+  background-color: #222222;
+  border-top: 5px #dd3333 solid;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+}
+.offcanvas-header .btn-close {
+  background-color: #ffffff !important;
+  font-size: 75%;
+}
+
+@media screen and (min-width: 768px) {
+  #sidebar {
+    width: 230px;
+    position: fixed;
+    height: 100vh;
+  }
+}
+</style>
