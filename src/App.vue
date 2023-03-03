7ecff7574d8561aea1781f7ff8468bc5d0dd0f22
Derek Woo <me@derekw.co>
2023-03-02 23:56:44 -0800

    Update global fonts and styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <SlideNavMenu />
   <PersistentNavMenu class="d-none d-md-block" />
   <content class="d-md-flex flex-row">
-    <router-view class="m-auto" />
+    <router-view class="w-100" />
   </content>
 </template>
 
@@ -40,20 +40,50 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   box-sizing: border-box;
   overflow-x: hidden;
+  font-size: 16px;
+}
+
+#content a {
+  color: #dd3333 !important;
+  text-decoration: none;
+}
+
+#content a:hover {
+  text-decoration: underline;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Lato", Helvetica, Arial, sans-serif;
+  font-weight: 700 !important;
+  margin-top: 30px !important;
+  line-height: 1.5em !important;
+}
+
+.wp-block-image {
+  text-align: center;
 }
 
 img,
 svg {
   max-width: 100%;
   height: auto;
+  margin: 10px;
 }
 
 @media screen and (min-width: 768px) {
