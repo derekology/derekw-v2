85f63368329b0b180af8c038174d1ca22a28c363
Derek Woo <me@derekw.co>
2023-03-12 20:52:55 -0700

    Add header logo to mobile top bar

--- src/components/navigation/TopNavBar.vue
+++ src/components/navigation/TopNavBar.vue
@@ -3,7 +3,7 @@
     <nav>
       <div class="navbar navbar-dark bg-dark d-md-none">
         <button
-          class="btn btn-primary ms-1"
+          class="btn btn-primary ms-1 bg-transparent"
           type="button"
           data-bs-toggle="offcanvas"
           data-bs-target="#mob-nav-collapse"
@@ -11,6 +11,13 @@
         >
           <menu-icon />
         </button>
+        <router-link
+          to="/"
+          class="nav-link text-white text-start text-center d-md-none"
+          aria-current="page"
+          ><HeaderLogoHorizontal />
+        </router-link>
+        <div style="width: 48px"></div>
       </div>
     </nav>
   </div>
@@ -19,14 +26,34 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import MenuIcon from "vue-material-design-icons/Menu.vue";
+import HeaderLogoHorizontal from "@/components/global/HeaderLogoHorizontal.vue";
 
 export default defineComponent({
   name: "TopNavBar",
 
   components: {
     MenuIcon,
+    HeaderLogoHorizontal,
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  border-top: 5px #dd3333 solid;
+  background-color: #222222 !important;
+  justify-content: space-between;
+  position: fixed;
+  width: 100%;
+}
+
+.btn {
+  /* background-color: #222222; */
+  border: 0px;
+}
+
+#site-logo-text {
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+}
+</style>
