80116e5533aba64ae7fff1b9da4d919e65d11d9d
Derek Woo <me@derekw.co>
2023-03-12 20:53:12 -0700

    Adjust single post styling

--- src/components/singlepost/SinglePostContent.vue
+++ src/components/singlepost/SinglePostContent.vue
@@ -19,18 +19,10 @@ export default defineComponent({
 <style scoped>
 #content {
   margin: 40px;
-  padding-top: 3em;
+  padding: 3em;
   max-width: 740px;
   text-align: left;
   font-size: 18px;
   line-height: 1.8em;
-}
-
-img,
-svg {
-  max-width: 100%;
-  height: auto;
-  margin-top: 1em;
-  margin-bottom: 2em;
 }
 </style>
